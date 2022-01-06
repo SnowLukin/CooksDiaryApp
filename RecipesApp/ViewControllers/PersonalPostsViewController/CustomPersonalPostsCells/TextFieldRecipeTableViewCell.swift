860763d1266e534e903a8837f4e105884dcd85ed
Anastasia Izmaylova <abulatova1992@gmail.com>
2022-01-06 22:42:28 +0300

    textViews in cells

--- RecipesApp/ViewControllers/PersonalPostsViewController/CustomPersonalPostsCells/TextFieldRecipeTableViewCell.swift
+++ RecipesApp/ViewControllers/PersonalPostsViewController/CustomPersonalPostsCells/TextFieldRecipeTableViewCell.swift
@@ -9,13 +9,12 @@ import UIKit
 
 class TextFieldRecipeTableViewCell: UITableViewCell {
     
-    @IBOutlet var textFieldRecipe: UITextField!
-
+    @IBOutlet var textFieldRecipe: UITextView!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         
         backgroundColor = UIColor(red: 0.224, green: 0.381, blue: 0.210, alpha: 1)
     }
-
-    
 }
+
--- RecipesApp/ViewControllers/PersonalPostsViewController/PersonalPostsTableViewController.swift
+++ RecipesApp/ViewControllers/PersonalPostsViewController/PersonalPostsTableViewController.swift
@@ -32,9 +32,11 @@ class PersonalPostsTableViewController: UITableViewController {
         switch indexPath.section {
         case 0:
             let cell = tableView.dequeueReusableCell(withIdentifier: "textFieldRecipeCell", for: indexPath) as! TextFieldRecipeTableViewCell
+            cell.textFieldRecipe.delegate = self
             return cell
         case 1:
            let cell = tableView.dequeueReusableCell(withIdentifier: "textFieldRecipeCell", for: indexPath) as! TextFieldRecipeTableViewCell
+            cell.textFieldRecipe.delegate = self
             return cell
         case 2:
             let cell = tableView.dequeueReusableCell(withIdentifier: "tagsRecipeCell", for: indexPath) as! TagsRecipeTableViewCell
@@ -90,51 +92,38 @@ class PersonalPostsTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
         return 30
     }
-    
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
+
+extension PersonalPostsTableViewController{
+    @objc private func didTapDone(){
+        view.endEditing(true)
+    }
+}
+
+extension PersonalPostsTableViewController: UITextViewDelegate{
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+    }
+    
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        let keyboardToolbar = UIToolbar()
+        keyboardToolbar.sizeToFit()
+        textView.inputAccessoryView = keyboardToolbar
+        
+        let doneButton = UIBarButtonItem(
+            barButtonSystemItem: .done,
+            target: self,
+            action: #selector(didTapDone)
+        )
+        
+        let flexBarButton = UIBarButtonItem(
+            barButtonSystemItem: .flexibleSpace,
+            target: nil,
+            action: nil
+        )
+        
+        keyboardToolbar.items = [flexBarButton, doneButton]
+    }
+}
+
